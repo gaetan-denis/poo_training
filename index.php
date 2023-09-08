30a2fe8c9458d5696261d7f2991a4e92c9968fa4
Gaëtan Denis <gaetandenis@outlook.be>
2023-09-08 21:24:30 +0200

    Added user display

--- index.php
+++ index.php
@@ -2,6 +2,37 @@
 
 require_once 'classes\user.php';
 
-$user1= new \classes\user(1,);
+$user1= new \classes\user(1,'Admin','1234');
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Php training</title>
+</head>
+<body>
+    <h1>Profile</h1>
+
+    <table>
+        <thead>
+        <tr>
+            <td>Id</td>
+            <td>Username</td>
+            <td>Password</td>
+        </tr>
+        </thead>
+        <tbody>
+            <td><?php echo $user1->getId()?></td>
+            <td><?php echo $user1->getUsername()?></td>
+            <td><?php echo $user1->getPassword()?></td>
+        </tbody>
+    </table>
+</body>
+</html>
 
 
